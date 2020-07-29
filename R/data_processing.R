2945a1068f2c5a3b653a78dda515b49f58f65674
axshirl <axshirl@gmail.com>
2020-07-28 22:15:34 -0500

    change to jsonlite

--- R/data_processing.R
+++ R/data_processing.R
@@ -1,6 +1,14 @@
-#ETL title stuff here
+##################################################
+## Project: SFV CPT Retrospective Analysis
+## Script purpose: Data Intkae & Processing
+## Date: 7/28/2020
+## Author: Alex Shirley, axshirl
+##################################################
 
-package_list = c("dplyr", "tidyr", "rjson")
+#### Package Loads ####
+package_list = c("dplyr", "tidyr", "jsonlite")
 packages_missing = package_list[!(package_list %in% installed.packages()[,"Package"])]
 if(length(packages_missing) > 0) install.packages(packages_missing)
 loaded_pkgs = lapply(package_list, require, character.only = TRUE)
+
+####Reading in json files####
