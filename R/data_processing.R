04b17f5d6de523696b8328bb437fead9f19aa00a
axshirl <axshirl@gmail.com>
2020-07-28 22:04:48 -0500

    Initial commit.

---
+++ R/data_processing.R
@@ -0,0 +1,6 @@
+#ETL title stuff here
+
+package_list = c("dplyr", "tidyr", "rjson")
+packages_missing = package_list[!(package_list %in% installed.packages()[,"Package"])]
+if(length(packages_missing) > 0) install.packages(packages_missing)
+loaded_pkgs = lapply(package_list, require, character.only = TRUE)
