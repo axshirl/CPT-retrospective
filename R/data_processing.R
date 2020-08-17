bcb68d21ed9be471ae0d95e578b81ea8707d2d30
axshirl <axshirl@gmail.com>
2020-08-16 21:00:41 -0500

    filtering to sfv

--- R/data_processing.R
+++ R/data_processing.R
@@ -6,14 +6,26 @@
 ##################################################
 
 #### Package Loads ####
-package_list = c("dplyr", "tidyr", "jsonlite")
+package_list = c("dplyr", "tidyr", "jsonlite", 'purrr')
 packages_missing = package_list[!(package_list %in% installed.packages()[,"Package"])]
 if(length(packages_missing) > 0) install.packages(packages_missing)
 loaded_pkgs = lapply(package_list, require, character.only = TRUE)
 
 ####Reading in json files####
-players_json <- fromJSON(file="data/players.json", simplify=TRUE) %>%
+tournaments_json <- fromJSON("data/tournaments.json") %>%
   as.data.frame()
 
-#turn json into tournament level- each record is a tournament tibble- have a column that is a list column of player involved, characters, placements
+#turn json into tournament level- each record is a tournament tibble-
+#have a column that is a list column of player involved, characters, placements
 #function takes in a nested structure
+
+#date read in as character
+sfv_tourneys <- tournaments_json %>% 
+  filter(version == 'SF5') %>%
+  mutate(date = as.Date(date))
+
+#need a way to gather a list of placements by character. 
+#Or even just setup average placement by character? mean and median?
+#group_by(pteams) %>% summarize(mean(place), median(place))
+#Big problem tho- We want to summarize these for ALL tournaments, we can't average them per tourney.
+#could unnest the players column then group & summarize. But I don't know if I like that... doesnt feel tidy.
