bd06f5d860aafbbb6a9a7a82d0943de001531d27
axshirl <axshirl@gmail.com>
2020-08-16 20:08:20 -0500

    reading json

--- R/data_processing.R
+++ R/data_processing.R
@@ -1,6 +1,6 @@
 ##################################################
 ## Project: SFV CPT Retrospective Analysis
-## Script purpose: Data Intkae & Processing
+## Script purpose: Data Intake & Processing
 ## Date: 7/28/2020
 ## Author: Alex Shirley, axshirl
 ##################################################
@@ -12,3 +12,8 @@ if(length(packages_missing) > 0) install.packages(packages_missing)
 loaded_pkgs = lapply(package_list, require, character.only = TRUE)
 
 ####Reading in json files####
+players_json <- fromJSON(file="data/players.json", simplify=TRUE) %>%
+  as.data.frame()
+
+#turn json into tournament level- each record is a tournament tibble- have a column that is a list column of player involved, characters, placements
+#function takes in a nested structure
