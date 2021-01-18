344a7059ed56e6d069eb50a4e769791048fb461d
axshirl <axshirl@gmail.com>
2021-01-18 13:52:56 -0600

    removing a block of old notes bc implemented

--- R/data_processing.R
+++ R/data_processing.R
@@ -109,19 +109,6 @@ tourney_results <- result_table %>% select('placing' = Placing,
                                            ) 
 
 
-
-#ooook. not a lot of actual code coming- gonna write up some quick findings.
-#2016-
-#-EVO: 1-1024/2-512/3-256/4-128/5-64/7-32/9-16/13-8
-#-Premier: 1-256/2-128/3-64/4-32/5-16/7-8/9-4/13-2
-#-Ranking: 1-128/2-64/3-32/4-16/5-8/7-4/9-2/13-1
-#Trend- EVO = 4x Premier, Premier = 2x Ranking.
-
-#2017-
-#-EVO: 1-1000/2-700/3-500/4-400/5-320/7-250/9-200/13-160/(decreases by 30 each time until)/49-40/65-20/97-10/129-5/193-1/
-#-Premier: 1-400/2-250/3-200/4-160/5-130/7-100/9-70/13-40/17-20/25-10/33-5/49-1
-#-Ranking: 1-160/2-100/3-70/4-40/5-20/7-10/9-5/13-1
-#sheesh. these get annoying. I think it might actually be smart to level up my scraping a bit
 #what I'm thinking is essentially
 # - we're already scraping each tourney name/class
 # - what if we scraped each results page (for possible tourneys?)
