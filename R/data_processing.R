d0f8bbca4af89bb20a244d45f871c0fa8375c4c9
axshirl <axshirl@gmail.com>
2021-02-06 05:30:17 -0600

    dreamhack temp fix

--- R/data_processing.R
+++ R/data_processing.R
@@ -84,13 +84,18 @@ for (i in 1:5) {
 #binding together the lists we just created 
 #& replacing the broken link for SCR 2016
 #(results page exists, but link referenced on the CPT Schedule page has a typo)
+#same problem exists for Dreamhack summer 2016... please let this be all!
 sfv_cpt = bind_rows(event_list) %>%
   mutate(event_results = replace(event_results, 
                                  event_year == 2016 & event_title == 'SoCal Regionals', 
                                  'https://capcomprotour.com/premier-event-socal-regionals-2016-results/'
                                  )
+         ) %>%
+  mutate(event_results = replace(event_results, 
+                                 event_year == 2016 & event_title == 'Dreamhack Summer', 
+                                 'https://capcomprotour.com/premier-tournament-dreamhack-summer-2016-results/'
+                                 )
          )
-
 #read in each results page and then start cleaning
 #lotta regex coming up. Need to separate sponsor tag from player tag
 #clean up Placing and change from character "1st" to numeric/int "1" 
